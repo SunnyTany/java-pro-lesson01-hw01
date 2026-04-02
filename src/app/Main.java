fd35ff9d857503028f6c989d6609b9471a6a7358
SunnyTany <46300377+SunnyTany@users.noreply.github.com>
2026-04-02 21:01:23 +0200

    add convPoundsToKgs

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,14 +4,19 @@ public class Main {
     public static final double CONV_K = 2.20462;
 
     public static void main(String[] args) {
-        System.out.println("Converter App");
+        System.out.println("Converter App.");
 
         double kgs = 5;
+        double pnds = 12;
         double pounds = convKgsToPounds(kgs);
-        System.out.println("Result is : " + pounds + "pounds.");
+        double kilos = convPoundsToKgs(pnds);
+        System.out.println("Result is : " + pounds + " pounds and " + kilos + " kilos");
     }
 
     private static double convKgsToPounds(double kgs) {
         return kgs * CONV_K;
+    }
+    private static double convPoundsToKgs(double pnds) {
+        return pnds / CONV_K;
     }
 }
